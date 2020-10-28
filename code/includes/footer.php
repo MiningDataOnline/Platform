3c8794f2c01e478f2a7b8f873859fde3a362ea35
AurelioSchug <49840461+AurelioSchug@users.noreply.github.com>
2020-10-28 23:50:12 +0100

    Create footer.php

---
+++ code/includes/footer.php
@@ -0,0 +1,25 @@
+
+		<!--  Scripts-->
+		<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+		<script src="/js/materialize.js"></script>
+		<script src="/js/init.js"></script>
+
+	</body>
+</html>
+
+
+<script>
+$(document).ready(function(){
+    $('select').formSelect();
+  });
+</script>
+
+
+<?php
+$conn->close();
+?>
+
+
+
+
+
