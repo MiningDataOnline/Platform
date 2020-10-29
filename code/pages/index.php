5f8c839aa08c0873166a0554fe12cb685641c75d
AurelioSchug <49840461+AurelioSchug@users.noreply.github.com>
2020-10-29 11:39:37 +0100

    Update index.php

--- code/pages/index.php
+++ code/pages/index.php
@@ -5,9 +5,8 @@ require_once("$path");
 ?>
 
 
-
+ <!--Embed logo stored in dropbox-->
  <center><img src="https://www.dropbox.com/s/ipzgo08y589xvsu/Screenshot%20from%202020-10-29%2000_05_07%281%29.png?raw=1" height="40%" width="40%"></center>
-
 
 
 <?php
