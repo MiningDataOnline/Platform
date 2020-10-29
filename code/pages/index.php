487d28113fd03923d1264543815b31a2aaf418cc
AurelioSchug <49840461+AurelioSchug@users.noreply.github.com>
2020-10-29 11:37:10 +0100

    Create index.php

---
+++ code/pages/index.php
@@ -0,0 +1,17 @@
+<?php
+$path = $_SERVER['DOCUMENT_ROOT'];
+$path .= "/code/includes/header.php";
+require_once("$path"); 
+?>
+
+
+
+ <center><img src="https://www.dropbox.com/s/ipzgo08y589xvsu/Screenshot%20from%202020-10-29%2000_05_07%281%29.png?raw=1" height="40%" width="40%"></center>
+
+
+
+<?php
+$path = $_SERVER['DOCUMENT_ROOT'];
+$path .= "/code/includes/footer.php";
+require_once("$path"); 
+?>
