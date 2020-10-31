dde613c66d5d190dfb4c2fdd61778fe9350ea49f
AurelioSchug <49840461+AurelioSchug@users.noreply.github.com>
2020-10-31 19:26:10 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,13 +5,20 @@ $path .= "/includes/header.php";
 require_once("$path"); 
 ?>
 
-
-<?php
-// include top navigation bar
-$path = $_SERVER['DOCUMENT_ROOT'];
-$path .= "/includes/nav_bar_top.php";
-include("$path"); 
-?>
+<div class="navbar-fixed">		
+	<nav class="#212121 grey darken-4" role="navigation">
+		<div class="nav-wrapper container"><a id="logo-container" href="/index.php" class="brand-logo">MiningDataOnline</a>
+			<ul class="right hide-on-med-and-down">			
+      <?php
+      // include top navigation bar
+      $path = $_SERVER['DOCUMENT_ROOT'];
+      $path .= "/includes/nav_bar_top.php";
+      include("$path"); 
+      ?>
+			</ul>
+		</div>						
+	</nav>			
+</div>	
 
 
  <!--Embed logo stored in dropbox-->
