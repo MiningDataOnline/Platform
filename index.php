d57917403578dfc7609fc952b035e799f49252dd
AurelioSchug <49840461+AurelioSchug@users.noreply.github.com>
2020-10-31 19:02:34 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,14 @@ require_once("$path");
 ?>
 
 
+<?php
+// include top navigation bar
+$path = $_SERVER['DOCUMENT_ROOT'];
+$path .= "/includes/top_nav_bar.php";
+require_once("$path"); 
+?>
+
+
  <!--Embed logo stored in dropbox-->
  <center><img src="https://www.dropbox.com/s/ipzgo08y589xvsu/Screenshot%20from%202020-10-29%2000_05_07%281%29.png?raw=1" height="30%" width="30%"></center>
 
