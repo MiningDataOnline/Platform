359fecd1ca665bc131fa1e7360dbcf70c5d9577e
AurelioSchug <49840461+AurelioSchug@users.noreply.github.com>
2020-11-01 00:44:05 +0100

    Update sign_up_page_1.php

--- user_account/sign_up_page_1.php
+++ user_account/sign_up_page_1.php
@@ -23,7 +23,7 @@ $path .= "/code/config/user_permissions.php";
 require("$path");
 */
 ?>
-	
+
 
 
 <?php
@@ -33,7 +33,7 @@ $path .= "/includes/header.html";
 include("$path"); 
 ?>
 
-	
+
 <?php
 // include top navigation bar
 $path = $_SERVER['DOCUMENT_ROOT'];
@@ -44,75 +44,75 @@ include("$path");
 
 
 
-				<h5 class="center">Sign Up</h5><br><br>
+<h5 class="center">Sign Up</h5><br><br>
 
 
-					<div class="row">
-					<div class="col s12 m12">
+<div class="row">
+	<div class="col s12 m12">
 
 
-							<?php
-							if($_SESSION["logedin"] == "yes"){
-							$user = $_SESSION["user"];
-							?>
-							<div align="center"><font size="3" color="#F0F0F0">You are currently signed in as "<?php echo $user?>"!</font></div><br>
-							<div align="center"><font size="3" color="#F0F0F0">You need to sign out before you can create a new account!</font></div><br>
-							<?php
-							}
-							else {
-							?>
-							<form action="sign_up2.php" method="post"> 
-								<div class="form-group"><br>					  
-									<table>
-										<col width="40%" /> 
-										<col width="60%" />
-										<tbody>
-											<tr>
-												<td valign="middle" style="padding-bottom: 2em;">
-													<div align="right"><font size="3" color="<?php echo $company_color?>">Username &emsp;</font></div>
-												</td>
-												<td valign="middle"  style="padding-bottom: 2em;">
-													<input style="background:#DEFFFF;color:#000000;" type="text" name="user"  value="" placeholder="Required" maxlength="25"  required>
-													<div align="left"><font size="1" color="#F0F0F0">Note 1: No white spaces allowed.</font></div>
-												</td>
-											</tr>
-											<tr>
-												<td valign="middle"  style="padding-bottom: 2em;">
-													<div align="right"><font size="3" color="<?php echo $company_color?>">Password &emsp;</font></div>
-												</td>
-												<td valign="middle"  style="padding-bottom: 2em;">
-													<input style="background:#DEFFFF;color:#000000;" type="password" name="pass1"  value="" placeholder="Required" maxlength="30"  required>
-													<div align="left"><font size="1" color="#F0F0F0">Note 2: No white spaces allowed;</font></div>
-													<div align="left"><font size="1" color="#F0F0F0">Note 3: Password must cointain at least 5 digits;</font></div>
-													<div align="left"><font size="1" color="#F0F0F0">Note 4: Your password must not be the same as your username.</font></div>
-												</td>
-											</tr>
-											<tr>
-												<td valign="middle"  style="padding-bottom: 2em;">
-													<div align="right"><font size="3" color="<?php echo $company_color?>"> Retype Password &emsp;</font></div>
-												</td>
-												<td valign="middle"  style="padding-bottom: 2em;">
-													<input style="background:#DEFFFF;color:#000000;"  type="password" name="pass2"  value="" placeholder="Required" maxlength="30"  required>
-												</td>
-											</tr>
-											<tr>
-												<td valign="middle" style="padding-bottom: 2em;">
-													<div align="right"><font size="3" color="<?php echo $company_color?>">E-Mail &emsp;</font></div>
-												</td>
-												<td valign="middle"  style="padding-bottom: 2em;">
-													<input style="background:#DEFFFF;color:#000000;" type="text" name="email"  value="" placeholder="Optional (recommended)"  maxlength="30" >
-												</td>
-											</tr>
-										</tbody>
-									</table>
+		<?php
+		if($_SESSION["logedin"] == "yes"){
+			$user = $_SESSION["user"];
+		?>
+		<div align="center"><font size="3" color="#F0F0F0">You are currently signed in as "<?php echo $user?>"!</font></div><br>
+		<div align="center"><font size="3" color="#F0F0F0">You need to sign out before you can create a new account!</font></div><br>
+		<?php
+		}
+		else {
+		?>
+		<form action="sign_up2.php" method="post"> 
+			<div class="form-group"><br>					  
+				<table>
+					<col width="40%" /> 
+					<col width="60%" />
+					<tbody>
+						<tr>
+							<td valign="middle" style="padding-bottom: 2em;">
+								<div align="right"><font size="3" color="<?php echo $company_color?>">Username &emsp;</font></div>
+							</td>
+							<td valign="middle"  style="padding-bottom: 2em;">
+								<input style="background:#DEFFFF;color:#000000;" type="text" name="user"  value="" placeholder="Required" maxlength="25"  required>
+								<div align="left"><font size="1" color="#F0F0F0">Note 1: No white spaces allowed.</font></div>
+							</td>
+						</tr>
+						<tr>
+							<td valign="middle"  style="padding-bottom: 2em;">
+								<div align="right"><font size="3" color="<?php echo $company_color?>">Password &emsp;</font></div>
+							</td>
+							<td valign="middle"  style="padding-bottom: 2em;">
+								<input style="background:#DEFFFF;color:#000000;" type="password" name="pass1"  value="" placeholder="Required" maxlength="30"  required>
+								<div align="left"><font size="1" color="#F0F0F0">Note 2: No white spaces allowed;</font></div>
+								<div align="left"><font size="1" color="#F0F0F0">Note 3: Password must cointain at least 5 digits;</font></div>
+								<div align="left"><font size="1" color="#F0F0F0">Note 4: Your password must not be the same as your username.</font></div>
+							</td>
+						</tr>
+						<tr>
+							<td valign="middle"  style="padding-bottom: 2em;">
+								<div align="right"><font size="3" color="<?php echo $company_color?>"> Retype Password &emsp;</font></div>
+							</td>
+							<td valign="middle"  style="padding-bottom: 2em;">
+								<input style="background:#DEFFFF;color:#000000;"  type="password" name="pass2"  value="" placeholder="Required" maxlength="30"  required>
+							</td>
+						</tr>
+						<tr>
+							<td valign="middle" style="padding-bottom: 2em;">
+								<div align="right"><font size="3" color="<?php echo $company_color?>">E-Mail &emsp;</font></div>
+							</td>
+							<td valign="middle"  style="padding-bottom: 2em;">
+								<input style="background:#DEFFFF;color:#000000;" type="text" name="email"  value="" placeholder="Optional (recommended)"  maxlength="30" >
+							</td>
+						</tr>
+					</tbody>
+				</table>
 
-									<center><button type="submit" name="submit2">Sign Up</button></center>
-								</div>
-							</form>
-						
-						<?php
-						}
-						?>
+				<center><button type="submit" name="submit2">Sign Up</button></center>
+			</div>
+		</form>
+
+		<?php
+		}
+		?>
 
 
 	</div>
