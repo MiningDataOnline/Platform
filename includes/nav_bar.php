acd056449810919909722d929e22e186f9d25da7
AurelioSchug <49840461+AurelioSchug@users.noreply.github.com>
2020-10-31 20:59:34 +0100

    Update nav_bar.php

--- includes/nav_bar.php
+++ includes/nav_bar.php
@@ -31,4 +31,9 @@
 				<li><a href="#">Navbar Link</a></li>
 			</ul>
 			<a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+			
+		</div>						
+	</nav>			
+</div>				
+			
 					
