65fcef26b44b6a03cc375628f484f3fa3a5bbb6a
AurelioSchug <49840461+AurelioSchug@users.noreply.github.com>
2020-10-31 20:48:23 +0100

    Update nav_bar_top.php

--- includes/nav_bar_top.php
+++ includes/nav_bar_top.php
@@ -1,4 +1,8 @@
 
+<div class="navbar-fixed">		
+	<nav class="#212121 grey darken-4" role="navigation">
+		<div class="nav-wrapper container"><a id="logo-container" href="/index.php" class="brand-logo">MiningDataOnline</a>
+			<ul class="right hide-on-med-and-down">
 
 
 
@@ -22,3 +26,11 @@
 			}
 			?>
 
+			</ul>
+			<ul id="nav-mobile" class="sidenav">
+				<li><a href="#">Navbar Link</a></li>
+			</ul>
+			<a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+		</div>						
+	</nav>			
+</div>						
