4190924dccd87c1453ddccdb6eed5e4a10cc4424
AurelioSchug <49840461+AurelioSchug@users.noreply.github.com>
2020-10-31 20:28:45 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,35 @@
 <?php
+// connect to database
+$path = $_SERVER['DOCUMENT_ROOT'];
+$path .= "/db_conn/db_1_start_conn.php";
+require("$path");
+?>
+
+
+
+
+<?php
+// start session
+session_start();
+$user = $_SESSION["user"];
+?>
+
+
+
+
+<?php
+/*
+// find out wich permissions the user has, in case the user is signed in
+$path = $_SERVER['DOCUMENT_ROOT'];
+$path .= "/code/config/user_permissions.php";
+require("$path");*/
+?>
+	
+	
+
+
+
+<?php
 // include header
 $path = $_SERVER['DOCUMENT_ROOT'];
 $path .= "/includes/header.php";
