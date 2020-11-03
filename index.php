ab0d2687ff8c95740fa15c4b0d5d33c4d509ebeb
AurelioSchug <49840461+AurelioSchug@users.noreply.github.com>
2020-11-04 00:05:46 +0100

    Update index.php

--- index.php
+++ index.php
@@ -50,15 +50,34 @@ include("$path");
  <center><img src="https://www.dropbox.com/s/ipzgo08y589xvsu/Screenshot%20from%202020-10-29%2000_05_07%281%29.png?raw=1" height="30%" width="30%"></center>
 
 
+
 <?php
+// check if users table exists... if not give the user the option to create an administrator account by sigining up. The users table is created when admin creates an account
+//  in case users table exists set $users_table=1 otherwise it will sta =0
 $query = "SELECT id FROM users";
 $result = mysqli_query($conn, $query);
 if(empty($result)) {
-?>
-<center><a href="/pages/user_account/sign_up_page_1.php" target='_parent'><font size="5" color="#5acefa">Get Started</font></a></center>
-<?php	
+	
+	?>
+	<center><a href="/pages/user_account/sign_up_page_1.php" target='_parent'><font size="5" color="#5acefa">Get Started</font></a></center>
+
+	<?php
+	// in case users table exits and the user is admin, do the following
+	if ($user=='admin') {
+
+	// check if table config exits.... if not, give admin the link to settings page	
+	$query = "SELECT id FROM config";
+	$result = mysqli_query($conn, $query);
+	if(empty($result)) {
+	?>
+	<center><a href="/pages/settings/settings.php" target='_parent'><font size="5" color="#5acefa">Settings</font></a></center>
+	<?php	
+	}
+	
+	}
+			
 }
-?>
+?>	
 
 
 
