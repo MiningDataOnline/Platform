7ce57de4a2d29cf4db46700bb64c47fc08c20d99
AurelioSchug <49840461+AurelioSchug@users.noreply.github.com>
2020-11-04 17:00:03 +0100

    Update sign_in_page_2.php

--- pages/user_account/sign_in_page_2.php
+++ pages/user_account/sign_in_page_2.php
@@ -51,66 +51,16 @@ include("$path");
 
 
 
-
-   <center>
+<center>
 
               <?php
               $username = mysqli_real_escape_string($conn, $_POST['username']);
               $pass = mysqli_real_escape_string($conn, $_POST['pass']);
 
 
-
-              $sql11 = "SELECT failed_attempts, email, failed_recover_attempts, pass FROM users WHERE user='$username' LIMIT 1";
-              $result11 = $conn->query($sql11);
-              if ($result11->num_rows > 0) {
-                while($row11 = $result11->fetch_assoc()) {
-                  $failed_attempts =  $row11["failed_attempts"];
-                  $failed_recover_attempts =  $row11["failed_recover_attempts"];
-                  $email =  $row11["email"];
-                  $passdb =  $row11["pass"];
-                }
-              }
-
-
-
-
-
-
-
               if($_SESSION["logedin"] == "yes"){
-                $user = $_SESSION["user"];
               ?>
-
               <div align="center"><font size="3" color="#F0F0F0">You are currently signed in as "<?php echo $user?>"!</font></div><br>
-
-              <?php
-              }
-              else if ($failed_attempts > 5) {
-                $sql = "UPDATE users SET failed_attempts = failed_attempts+1 WHERE user='$username'";
-                mysqli_query($conn, $sql);
-              ?>
-              <br><br>
-              <font size="3" color="#F0F0F0">This account has been blocked because of too many failed attempts to log in!</font><br><br>
-
-              <font size="1" color="#F0F0F0">Note: To many failed attempts to log in could cause us to block your IP-Address for security reasons! Please quit trying to log in after your account has been blocked! Try to recover your account (if this function is still available for your account) to unblock your account</font><br><br>
-
-              <?php
-                if ($failed_recover_attempts  >= 5) {
-                }
-                else
-                {
-              ?>
-              <a href="recover_account.php?username=<?php echo $username; ?>"><font size="3" color="#81DAF5">Recover Account</font></a><br><br>
-              <font size="3" color="#81DAF5">OR</font><br><br>
-              <?php
-                }
-              ?>
-
-
-
-              <a href="/home.php"><font size="3" color="#81DAF5">Go To Homepage</font></a><br><br>
-              
-              
               <?php
               }
               else if (password_verify($pass, $passdb)) {
@@ -132,21 +82,14 @@ include("$path");
                 echo "Upps! Wrong username or password!";
                 ?> 
               </font>
-
-              <br><br><br>
+              <br><br>
               <a href="sign_in.php"><font size="3" color="#81DAF5">Try Again</font></a><br><br>
               <?php
-
-
               }
-
-
               ?> 
-              <br><br>
 
 
-
-            </center>
+</center>
 
 
 
