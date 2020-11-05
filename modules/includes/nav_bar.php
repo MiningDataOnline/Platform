a45e2702ca97411dbea005ffca344246a8d81c64
AurelioSchug <49840461+AurelioSchug@users.noreply.github.com>
2020-11-05 18:15:11 +0100

    Update nav_bar.php

--- modules/includes/nav_bar.php
+++ modules/includes/nav_bar.php
@@ -42,9 +42,9 @@ $category =  $row["category"];
 
 	<nav class="#212121 grey darken-4" role="navigation">
 			<div class="nav-wrapper container"><a id="logo-container" href="/index.php" class="brand-logo"><font color="#5acefa"><?php echo $category?></font></a>
-	<br><br><br>
 	 <!--Embed logo stored in dropbox-->
-	 <center><img src="https://www.dropbox.com/s/ipzgo08y589xvsu/Screenshot%20from%202020-10-29%2000_05_07%281%29.png?raw=1"></center>
+	 <center><img src="https://www.dropbox.com/s/ipzgo08y589xvsu/Screenshot%20from%202020-10-29%2000_05_07%281%29.png?raw=1" height="0.5%" width="0.5%"></center>
+
 
 			<ul class="right hide-on-med-and-down">
 
