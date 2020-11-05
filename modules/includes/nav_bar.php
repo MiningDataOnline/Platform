00019986283679e233c41debc77592b90bf6e4ca
AurelioSchug <49840461+AurelioSchug@users.noreply.github.com>
2020-11-05 18:36:03 +0100

    Update nav_bar.php

--- modules/includes/nav_bar.php
+++ modules/includes/nav_bar.php
@@ -41,6 +41,48 @@ $category =  $row["category"];
 <div class="navbar-fixed">	
 
 	<nav class="#212121 grey darken-4" role="navigation">
+		
+		
+		
+		
+		
+		
+		
+			<ul class="left hide-on-med-and-down">
+
+
+			<?php
+			// 
+			// if user is logged in show username, a link to "my account" and the option to sign out.
+			// if user is not logged in, show a link to the "sign in" and the "sign up" form
+	
+
+			if($_SESSION["logedin"] == "yes"){
+			?>
+			<li><font size="2" color="grey">User: </font><font size="2" color="grey"><?php echo $user?></font></li>
+			<li><a href="/pages/user_account/my_account.php" target='_parent'><font size="2" color="#5acefa">My Account</font></a></li>
+			<li><a href="/pages/user_account/sign_out.php" target='_parent'><font size="2" color="#5acefa">Sign Out</font></a></li>
+			<?php
+			} else {
+			?>
+			<li><a href="/pages/user_account/sign_in_page_1.php" target='_parent'><font size="2" color="#5acefa">Sign In</font></a></li>
+			<li><a href="/pages/user_account/sign_up_page_1.php" target='_parent'><font size="2" color="#5acefa">Sign Up</font></a></li>
+			<?php
+			}
+			?>
+
+			</ul>		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 			<ul class="right hide-on-med-and-down">
 
 
