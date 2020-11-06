29c1891b9b7ed4403f64f3cc37417a01e29e45ac
AurelioSchug <49840461+AurelioSchug@users.noreply.github.com>
2020-11-06 08:53:57 +0100

    Update add_subcategory.php

--- modules/includes/add_subcategory.php
+++ modules/includes/add_subcategory.php
@@ -5,7 +5,6 @@ if (isset($_POST["add_subcategory"])) {
 $editdate = date("ymd"); 
 $nowDate = gmdate("Ym"); 
 $subcategory_name = mysqli_real_escape_string($conn, $_POST['subcategory_name']);
-echo $subcategory_name;
 $sql = "INSERT INTO subcategories (subcategory_name, subcategory_name_editor, subcategory_name_editdate,  subcategory_name_bill_month_year) VALUES('$subcategory_name', '$user', '$editdate', '$nowDate')";  
 mysqli_query($conn, $sql); 
 }
