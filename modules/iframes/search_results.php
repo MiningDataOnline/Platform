b0ddcc4ff35ed39e76db8bbca23aae4bd37db53e
AurelioSchug <49840461+AurelioSchug@users.noreply.github.com>
2020-11-06 08:15:25 +0100

    Update search_results.php

--- modules/iframes/search_results.php
+++ modules/iframes/search_results.php
@@ -60,6 +60,43 @@ require_once("$path");
 
 
 
+
+
+
+
+
+<?php
+$editdate = date("ymd"); 
+if (!empty($edidate)){
+$nowDate = gmdate("Ym"); 
+$subcategory_name = mysqli_real_escape_string($conn, $_POST['$subcategory_name']);
+$sql = "INSERT INTO categories (subcategory_name, subcategory_name_editor, subcategory_name_editdate,  subcategory_name_bill_month_year) VALUES('$subcategory_name', '$user', '$editdate', '$nowDate')";  
+mysqli_query($conn, $sql); 
+}
+?>
+
+
+<form action="search_results.php" method="post" enctype="multipart/form-data"> 
+<div class="form-group" >
+<table class="table"  >
+<tbody>
+<tr>
+<th scope="row"></th>
+<td>
+<center><font size="5" color="#D8D8D8" >Project Name:</font></center>
+</td>
+<td>
+<input style="background:#DEFFFF;color:#000000;" class="form-control name_list" name="subcategory_name" size="40"  maxlength="50" value="" placeholder="Enter Project Name" required/><br>
+</td>
+</tr>
+</tbody>
+</table>
+<center><input type="submit" value="add_subcategory" name="submit"></center><br>  
+</div>
+</form> 
+
+
+
 <?php
 // search and show all subcategories 
 // editor, auditor and admin get the variable with the option to change and verrify them
@@ -68,54 +105,63 @@ $sql55 = "SELECT id FROM subcategories";
 $result55 = $conn->query($sql55);
 if ($result55->num_rows > 0) {
     while($row55 = $result55->fetch_assoc()) {
-			$id = $row55["id"];
-			$output1 = '/project_pages/ownership/project_ownership.php?id=';
-			$output2 = $output1 . $id;
-		
-      <?php
-      // name
-      $variable='subcategory_name';
-      // table
-      $table='subcategories';
-      //name of ID columm
-      $id_column_name='id';
-      //Name of input-ID
-      $input_id=$id;
-      //Name of input-ID
-      $input_id_name='id';
-      //show variable for admin + contributers?
-      $show_intern='yes';
-      //show variable for public view?
-      $show_public='no';	
-      //show variable input field?
-      $show_input='yes';			
-      //show unit input field?
-      $show_input_unit='no';
+	$id = $row55["id"];
+	$output1 = '/project_pages/ownership/project_ownership.php?id=';
+	$output2 = $output1 . $id;
 
-      $show_input_source='no';	
-      $show_source_information_words='no';
-
-      // wenn nur admin verifizieren darf
-      $admin_audit_only='yes';																																				   
-
-      //Data type: int, varchar, text
-      $variable_data_type='varchar';	
-      $variable_max_lenght=50;	
-
-      $back_page='/project_pages/search/searchresults_show_all.php?'.$input_id_name.'='.$id;		
-      $path = $_SERVER['DOCUMENT_ROOT'];
-      $path .= "/modules/variables_varchar.php";
-      require("$path");
-      $name=$variable_value;
-      $name_activate=$variable_value;
-      $name_activate_source=$variable_source_value;
-      ?>		   
     
-    
+	    
+	    
+
+
+// name
+$variable='subcategory_name';
+// table
+$table='subcategories';
+//name of ID columm
+$id_column_name='id';
+//Name of input-ID
+$input_id=$id;
+//Name of input-ID
+$input_id_name='id';
+//show variable for admin + contributers?
+$show_intern='yes';
+//show variable for public view?
+$show_public='no';	
+//show variable input field?
+$show_input='yes';			
+//show unit input field?
+$show_input_unit='no';
+
+$show_input_source='no';	
+$show_source_information_words='no';
+
+// wenn nur admin verifizieren darf
+$admin_audit_only='yes';																																				   
+
+//Data type: int, varchar, text
+$variable_data_type='varchar';	
+$variable_max_lenght=50;	
+
+$back_page='/project_pages/search/searchresults_show_all.php?'.$input_id_name.'='.$id;		
+$path = $_SERVER['DOCUMENT_ROOT'];
+$path .= "/modules/variables_varchar.php";
+require("$path");
+$name=$variable_value;
+$name_activate=$variable_value;
+$name_activate_source=$variable_source_value;
+?>		   
+	    
+	    
+	    
+	    
+	    
+	    
+	    
       <?php    
       
-			if ($name_activate == 3 || $user == 'admin'){
-			?>
+	if ($name_activate == 3 || $user == 'admin'){
+	?>
             <br><br>					
             <center><a  class ="deep-orange-text" href="<?php echo $output2;?>"><font size="3"><?php echo $name; ?></font></a></center><br>		
       <?php
