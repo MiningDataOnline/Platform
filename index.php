125c731f5a5812916a231af0073c59306e76cbe3
AurelioSchug <49840461+AurelioSchug@users.noreply.github.com>
2020-11-07 00:06:13 +0100

    Update index.php

--- index.php
+++ index.php
@@ -101,7 +101,7 @@ include("$path");
 				
 				
 				
- <iframe src="/modules/iframes/search_results.php"  height="50%" ></iframe> 				
+ <iframe src="/modules/iframes/search_results.php"  height="200px" ></iframe> 				
 
 
 			</div>  
