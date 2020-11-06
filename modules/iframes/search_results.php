b924cfb3c7de7aef28e40e8f50285cbd4a84ddfd
AurelioSchug <49840461+AurelioSchug@users.noreply.github.com>
2020-11-06 08:40:27 +0100

    Update search_results.php

--- modules/iframes/search_results.php
+++ modules/iframes/search_results.php
@@ -40,66 +40,6 @@ include("$path");
 
 
 
-
-
-
-
-<?php
-// create subcategory table
-$path = $_SERVER['DOCUMENT_ROOT'];
-$path .= "/modules/requires/create_subcategories_table.php";
-require_once("$path");
-?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<?php
-$submit = mysqli_real_escape_string($conn, $_POST['submit']);
-if (!empty($submit)){
-	
-$editdate = date("ymd"); 
-$nowDate = gmdate("Ym"); 
-$subcategory_name = mysqli_real_escape_string($conn, $_POST['$subcategory_name']);
-	echo $subcategory_name;
-$sql = "INSERT INTO subcategories (subcategory_name, subcategory_name_editor, subcategory_name_editdate,  subcategory_name_bill_month_year) VALUES('$subcategory_name', '$user', '$editdate', '$nowDate')";  
-mysqli_query($conn, $sql); 
-}
-?>
-
-
-<form action="search_results.php" method="post" enctype="multipart/form-data"> 
-<div class="form-group" >
-<table class="table"  >
-<tbody>
-<tr>
-<th scope="row"></th>
-<td>
-<center><font size="5" color="#D8D8D8" >Add Subcategory:</font></center>
-</td>
-<td>
-<input style="background:#DEFFFF;color:#000000;" class="form-control name_list" name="subcategory_name" size="40"  maxlength="50" value="" placeholder="Enter Project Name" required/><br>
-</td>
-</tr>
-</tbody>
-</table>
-<center><input type="submit" value="submit" name="submit"></center><br>  
-</div>
-</form> 
-
-
-
 <?php
 // search and show all subcategories 
 // editor, auditor and admin get the variable with the option to change and verrify them
