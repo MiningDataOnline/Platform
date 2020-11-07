6a5e2b747ef759b08494b433f26f7bf02061bef9
AurelioSchug <49840461+AurelioSchug@users.noreply.github.com>
2020-11-07 14:09:54 +0100

    Update index.php

--- index.php
+++ index.php
@@ -95,7 +95,7 @@ include("$path");
 	<div class="col s12 m9 right">
 
 
-			<center><iframe src="/pages/iframes/start_page.php" height="800px" width="100%"  allowtransparency="true" ></iframe></center> 
+			<center><iframe src="/pages/iframes/start_page.php" height="800px" width="95%"  allowtransparency="true" ></iframe></center> 
 
 		
 		
