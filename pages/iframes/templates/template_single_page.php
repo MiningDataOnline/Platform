921706de58bedb504df6c50b5d8eb6d800d92493
AurelioSchug <49840461+AurelioSchug@users.noreply.github.com>
2020-11-07 19:46:23 +0100

    Update template_single_page.php

--- pages/iframes/templates/template_single_page.php
+++ pages/iframes/templates/template_single_page.php
@@ -99,7 +99,7 @@ $subcategory_name =  $row["subcategory_name"];
 
 				if ($show_public=='yes' || $show_intern=='yes' && ($company_editor == 1 || $company_auditor == 1  ||  $user == 'administrador')){
 				?>				  	  
-				<font size="3" color="grey">Book:</font>	
+				<font size="3" color="grey">Book: </font>	
 				<?php 				
 				} 	
 
@@ -142,7 +142,7 @@ $subcategory_name =  $row["subcategory_name"];
 	
 				if ($show_public=='yes' || $show_intern=='yes' && ($company_editor == 1 || $company_auditor == 1  ||  $user == 'administrador')){
 				?>				  	  
-				<font size="3" color="grey"><?php echo $variable; ?></font>	
+				<font size="3" color="grey"><?php echo $variable; ?>: </font>	
 				<?php 				
 				} 	
 	
