30e8960f26d63a7f2a5a23672bbeb037185f4a94
AurelioSchug <49840461+AurelioSchug@users.noreply.github.com>
2020-11-07 15:00:25 +0100

    Update and rename pages/iframes/template_single_page.php to pages/iframes/templates/template_single_page.php

---
+++ pages/iframes/templates/template_single_page.php
@@ -0,0 +1,140 @@
+
+<?php
+// connect to database db_1
+$path = $_SERVER['DOCUMENT_ROOT'];
+$path .= "/db_conn/conn.php";
+require_once("$path");
+?>
+
+
+
+
+<?php
+// start user session
+session_start();
+$user = $_SESSION["user"];
+?>
+
+
+
+
+<?php
+/*
+// find out wich permissions the user has, in case the user is signed in
+$path = $_SERVER['DOCUMENT_ROOT'];
+$path .= "/code/config/user_permissions.php";
+require("$path");*/
+?>
+	
+	
+
+
+
+<?php
+// include header
+$path = $_SERVER['DOCUMENT_ROOT'];
+$path .= "/modules/includes/header.php";
+include("$path"); 
+?>
+
+
+
+<?php
+$subcategory_id = clear($_GET['subcategory_id']);
+if (empty($company_id))
+{
+$subcategory_id = clear($_POST['subcategory_id']);
+}
+?>
+
+
+
+<?php
+// get subcategory name and show it
+$sql = "SELECT category_name FROM subcategories WHERE ID='$subcategory_id'";
+$result4 = $conn->query($sql);
+if ($result4->num_rows > 0) {
+while($row = $result4->fetch_assoc()) {
+$category_name =  $row["category_name"];
+}
+}
+?>
+<center><font size="8"  class ="deep-orange-text" ><?php echo $name; ?></font></center><br><br>
+
+
+
+
+
+
+
+
+
+
+
+
+
+															
+	
+        <?php
+				// variable name
+				$variable='paragraph_1';
+				// table
+				$table='template_single_page';
+				//name of ID columm
+				$id_column_name='id';
+				//Name of input-ID
+				$input_id=$subcategory_id;
+				//Name of input-ID
+				$input_id_name='subcategory_id';
+				//show variable for admin + contributers?
+				$show_intern='yes';
+				//show variable for public view?
+				$show_public='yes';	
+				//show variable input field?
+				$show_input='yes';			
+				//show unit input field?
+				$show_input_unit='no';
+				//Data type: int, varchar, text
+					 $variable_data_type='varchar';	
+					$variable_max_lenght=5000;						
+				$back_page='/pages/iframes/template_single_page.php?'.$input_id_name.'='.$input_id;		
+				$path = $_SERVER['DOCUMENT_ROOT'];
+				$path .= "/modules/variables_varchar.php";
+				require("$path");
+				$paragraph_1=$variable_value;
+				$paragraph_1_source=$variable_source_value;
+        ?>																	
+																
+																
+				
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+// include footer
+$path = $_SERVER['DOCUMENT_ROOT'];
+$path .= "/modules/includes/footer.php";
+include ("$path"); 
+?>
+
+
+
+<?php
+// close db connection
+$conn->close();
+?>
