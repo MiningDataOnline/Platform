c947ff60a200a55ffb0406ecb0176fe27082ca03
AurelioSchug <49840461+AurelioSchug@users.noreply.github.com>
2020-11-08 12:13:19 +0100

    Update search_results.php

--- pages/iframes/search_results.php
+++ pages/iframes/search_results.php
@@ -69,24 +69,120 @@ $sql55 = "SELECT * FROM subcategories";
 $result55 = $conn->query($sql55);
 if ($result55->num_rows > 0) {
     while($row55 = $result55->fetch_assoc()) {
-	$id = $row55["ID"];
-	$subcategory_name = $row55["subcategory_name"];
+	$subcategory_id = $row55["ID"];
 	$link = '/pages/iframes/templates/'.$template.'.php?subcategory_id='.$id;
 	?>		   
 	    
-	    
+
+
+	
+	
+	
 	    
 	    
       	<?php    
 	if ($name_activate == 3 || $user == 'admin'){
 	?>
-      	<br><br>					
+      	<br><br>
+	
+	
+	
+
+        			<?php
+				// Name of the variable (the name will be viewable). It's not used for nothing other than to name the variable in a readable form
+				$variable_designation='Subcategory';
+				// variable name
+				$variable='subcategory_name';
+				// table
+				$table='subcategories';
+				//name of ID columm
+				$id_column_name='ID';
+				//Name of input-ID
+				$input_id=$subcategory_id;
+				//Name of input-ID
+				$input_id_name='subcategory_id';
+				//show variable for admin + contributers?
+				$show_intern='yes';
+				//show variable for public view?
+				$show_public='yes';	
+				//show variable input field?
+				$show_input='yes';			
+				//show unit input field?
+				$show_input_unit='no';
+				//Data type: int, varchar, text
+				$variable_data_type='varchar';	
+				$variable_max_lenght=50;
+				$variable_font_color='#5acefa';
+				if ($show_public=='yes' || $show_intern=='yes' && ($company_editor == 1 || $company_auditor == 1  ||  $user == 'administrador')){
+				?>				  	  
+				<font size="3" color="grey"><?php echo $variable_designation; ?></font>	
+				<?php 				
+				} 	
+
+				$back_page='/pages/iframes/templates/template_single_page.php?'.$input_id_name.'='.$input_id;		
+				$path = $_SERVER['DOCUMENT_ROOT'];
+				$path .= "/modules/variable_varchar.php";
+				include("$path");
+        			?>		
+	
+	
+	
+	
             <center><a  class ="deep-orange-text" href="<?php echo $link;?>" target='pages_iframe'><font size="3"><?php echo $subcategory_name; ?></font></a></center><br>		
-     	 <?php
+     	
+	
+	
+	<?php
             }
       	else if($name_activate == 2)
-      	{?>									
-            <center><font size="3" color="grey"><?php echo $subcategory_name; ?></font></center><br>	
+      	{?>	
+	
+	
+	
+
+	
+
+        			<?php
+				// Name of the variable (the name will be viewable). It's not used for nothing other than to name the variable in a readable form
+				$variable_designation='Subcategory';
+				// variable name
+				$variable='subcategory_name';
+				// table
+				$table='subcategories';
+				//name of ID columm
+				$id_column_name='ID';
+				//Name of input-ID
+				$input_id=$subcategory_id;
+				//Name of input-ID
+				$input_id_name='subcategory_id';
+				//show variable for admin + contributers?
+				$show_intern='yes';
+				//show variable for public view?
+				$show_public='yes';	
+				//show variable input field?
+				$show_input='yes';			
+				//show unit input field?
+				$show_input_unit='no';
+				//Data type: int, varchar, text
+				$variable_data_type='varchar';	
+				$variable_max_lenght=50;
+				$variable_font_color='#5acefa';
+				if ($show_public=='yes' || $show_intern=='yes' && ($company_editor == 1 || $company_auditor == 1  ||  $user == 'administrador')){
+				?>				  	  
+				<font size="3" color="grey"><?php echo $variable_designation; ?></font>	
+				<?php 				
+				} 	
+
+				$back_page='/pages/iframes/templates/template_single_page.php?'.$input_id_name.'='.$input_id;		
+				$path = $_SERVER['DOCUMENT_ROOT'];
+				$path .= "/modules/variable_varchar.php";
+				include("$path");
+	 			$subcategory_name_value=$variable_value;
+        			?>			
+	
+	
+	
+           			 <center><font size="3" color="grey"><?php echo $subcategory_name_value; ?></font></center><br>	
       	<?php
       	} 
       	else 
