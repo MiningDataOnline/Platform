40aaf61761acc8ece80536a2cd76c5514a66450c
AurelioSchug <49840461+AurelioSchug@users.noreply.github.com>
2020-11-09 20:21:44 +0100

    Create conn.php

---
+++ conn_db/conn.php
@@ -0,0 +1,11 @@
+<?php
+//Database Host
+$db_host = 'localhost';
+//Database Name
+$db_database = 'xxxxx';
+//Database Username
+$db_user = 'xxxxx';
+//Database Password
+$db_pass = 'xxxxxx';
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_database);
+?>
