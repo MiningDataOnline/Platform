3278eecc2831ebbe8bdaf0c6639d5a56c9ce1395
AurelioSchug <49840461+AurelioSchug@users.noreply.github.com>
2020-11-09 20:56:00 +0100

    Update conn.php

--- db_conn/conn.php
+++ db_conn/conn.php
@@ -4,7 +4,7 @@ $db_host = 'localhost';
 //Database Name
 $db_database = 'mdo_db_1';
 //Database Username
-$db_user = 'xxxxx';
+$db_user = 'mydatabase';
 //Database Password
 $db_pass = 'xxxxxx';
 $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_database);
